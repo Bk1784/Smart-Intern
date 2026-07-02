4af7906066661c4b7a1bc3dd6de4c2996ba39bc4
Bk1784 <futonabuu456@gmail.com>
2026-07-02 22:03:41 +0700

    menambahkan flatpickr di update.blade

--- resources/views/_admin/logbook/update.blade.php
+++ resources/views/_admin/logbook/update.blade.php
@@ -16,7 +16,9 @@
                     <label class="block text-sm font-medium mb-2 text-gray-800 dark:text-neutral-200">
                         Tanggal
                     </label>
-                    <x-admin.input type="date" name="tanggal" :value="old('tanggal', $logbook->tanggal->format('Y-m-d'))" />
+                    <x-admin.input type="text" name="tanggal" id="tanggal-input" autocomplete="off"
+                        :value="old('tanggal', $logbook->tanggal->format('Y-m-d'))"
+                        placeholder="Pilih tanggal..." />
                     @error('tanggal')
                         <p class="mt-1 text-sm text-red-600">{{ $message }}</p>
                     @enderror
@@ -35,7 +37,6 @@
                 </div>
 
             </div>
-            <br>
 
             <div class="mt-5 flex items-center gap-x-2">
                 <x-admin.button type="submit" color="primary" class="font-bold">
@@ -47,4 +48,20 @@
             </div>
         </form>
     </div>
+
+    @push('scripts')
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+        <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
+        <script src="https://cdn.jsdelivr.net/npm/flatpickr/dist/l10n/id.js"></script>
+
+        <script>
+            flatpickr("#tanggal-input", {
+                dateFormat: "Y-m-d",
+                altInput: true,
+                altFormat: "d F Y",
+                locale: "id",
+                maxDate: "today",
+            });
+        </script>
+    @endpush
 @endsection
